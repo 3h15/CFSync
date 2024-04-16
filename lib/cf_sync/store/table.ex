2cdda8270fcea302eb832fb0922075daf75bbb57
Cyprien Poisson <c.poisson@cpi-services.fr>
2024-04-16 17:28:41 +0200

    Table: handle locale.

--- lib/cf_sync/store/table.ex
+++ lib/cf_sync/store/table.ex
@@ -42,20 +42,23 @@ defmodule CFSync.Store.Table do
   end
 
   @spec put(:ets.tid(), CFSync.Asset.t() | CFSync.Entry.t()) :: true
-  def put(ref, %Entry{id: id} = entry), do: put(ref, :entry, id, entry.content_type, entry)
-  def put(ref, %Asset{id: id} = asset), do: put(ref, :asset, id, :asset, asset)
+  def put(ref, %Entry{id: id, locale: locale} = entry),
+    do: put(ref, :entry, id, locale, entry.content_type, entry)
 
-  @spec get_entry(:ets.tid(), binary()) :: nil | Entry.t()
-  def get_entry(ref, entry_id) do
-    case :ets.lookup(ref, {:entry, entry_id}) do
+  def put(ref, %Asset{id: id, locale: locale} = asset),
+    do: put(ref, :asset, id, locale, :asset, asset)
+
+  @spec get_entry(:ets.tid(), binary(), atom()) :: nil | Entry.t()
+  def get_entry(ref, entry_id, locale) do
+    case :ets.lookup(ref, {:entry, entry_id, locale}) do
       [{_key, _content_type, entry}] -> entry
       _ -> nil
     end
   end
 
-  @spec get_asset(:ets.tid(), binary()) :: nil | Asset.t()
-  def get_asset(ref, asset_id) do
-    case :ets.lookup(ref, {:asset, asset_id}) do
+  @spec get_asset(:ets.tid(), binary(), atom()) :: nil | Asset.t()
+  def get_asset(ref, asset_id, locale) do
+    case :ets.lookup(ref, {:asset, asset_id, locale}) do
       [{_key, _content_type, asset}] -> asset
       _ -> nil
     end
@@ -79,36 +82,36 @@ defmodule CFSync.Store.Table do
     end
   end
 
-  @spec get_entries_for_content_type(:ets.tid(), atom) :: list
-  def get_entries_for_content_type(ref, content_type) do
-    records = :ets.match_object(ref, {:_, content_type, :_})
+  @spec get_entries_for_content_type(:ets.tid(), locale :: atom, content_type :: atom) :: list
+  def get_entries_for_content_type(ref, locale, content_type) do
+    records = :ets.match_object(ref, {{:_, :_, locale}, content_type, :_})
 
     for {_key, _content_type, entry} <- records do
       entry
     end
   end
 
-  @spec get_link_target(:ets.tid(), Link.t()) :: nil | Entry.t() | Asset.t()
-  def get_link_target(store, %Link{type: :entry, id: id}) do
-    get_entry(store, id)
+  # @spec get_link_target(:ets.tid(), Link.t()) :: nil | Entry.t() | Asset.t()
+  # def get_link_target(store, %Link{type: :entry, id: id}) do
+  #   # get_entry(store, id)
+  # end
+
+  # def get_link_target(store, %Link{type: :asset, id: id}) do
+  #   # get_asset(store, id)
+  # end
+
+  @spec delete_entry(:ets.tid(), binary(), atom()) :: true
+  def delete_entry(ref, entry_id, entry_locale) do
+    :ets.delete(ref, {:entry, entry_id, entry_locale})
   end
 
-  def get_link_target(store, %Link{type: :asset, id: id}) do
-    get_asset(store, id)
+  @spec delete_asset(:ets.tid(), binary(), atom()) :: true
+  def delete_asset(ref, asset_id, asset_locale) do
+    :ets.delete(ref, {:asset, asset_id, asset_locale})
   end
 
-  @spec delete_entry(:ets.tid(), binary()) :: true
-  def delete_entry(ref, entry_id) do
-    :ets.delete(ref, {:entry, entry_id})
-  end
-
-  @spec delete_asset(:ets.tid(), binary()) :: true
-  def delete_asset(ref, asset_id) do
-    :ets.delete(ref, {:asset, asset_id})
-  end
-
-  defp put(ref, type, id, content_type, record) do
-    key = {type, id}
+  defp put(ref, type, id, locale, content_type, record) do
+    key = {type, id, locale}
     :ets.insert(ref, {key, content_type, record})
   end
 end
--- test/cf_sync/store/table_test.exs
+++ test/cf_sync/store/table_test.exs
@@ -14,9 +14,7 @@ defmodule CFSync.Store.TableTest do
   end
 
   test "new/1 creates a protected ordered_set table, with read concurrency and no write concurrency" do
-    name = Faker.Util.format("%A%4a%A%3a") |> String.to_atom()
-    reference = Table.new(name)
-
+    reference = create_table()
     info = :ets.info(reference)
 
     assert Keyword.fetch!(info, :type) == :ordered_set
@@ -26,106 +24,98 @@ defmodule CFSync.Store.TableTest do
   end
 
   test "put/2 inserts entries in table" do
-    name = Faker.Util.format("%A%4a%A%3a") |> String.to_atom()
-    reference = Table.new(name)
-
-    entry = %{id: id} = create_page_entry()
+    reference = create_table()
+    entry = create_page_entry()
     Table.put(reference, entry)
-
-    assert [{_, _, %Entry{id: ^id}}] = :ets.lookup(reference, {:entry, entry.id})
+    assert [{_, _, ^entry}] = :ets.lookup(reference, {:entry, entry.id, entry.locale})
   end
 
   test "put/2 inserts assets in table" do
-    name = Faker.Util.format("%A%4a%A%3a") |> String.to_atom()
-    reference = Table.new(name)
-
-    asset = %{id: id} = create_asset()
+    reference = create_table()
+    asset = create_asset()
     Table.put(reference, asset)
-
-    assert [{_, _, %Asset{id: ^id}}] = :ets.lookup(reference, {:asset, asset.id})
+    assert [{_, _, ^asset}] = :ets.lookup(reference, {:asset, asset.id, asset.locale})
   end
 
   test "get_entry/2 get entry from table" do
-    name = Faker.Util.format("%A%4a%A%3a") |> String.to_atom()
-    reference = Table.new(name)
-
-    entry = %{id: id} = create_page_entry()
+    reference = create_table()
+    entry = create_page_entry()
     Table.put(reference, entry)
-
-    assert Table.get_entry(reference, id) == entry
+    assert Table.get_entry(reference, entry.id, entry.locale) == entry
   end
 
   test "delete_entry/2 deletes entry from table" do
-    name = Faker.Util.format("%A%4a%A%3a") |> String.to_atom()
-    reference = Table.new(name)
-
-    entry_1 = %{id: id_1} = create_page_entry()
-    entry_2 = %{id: id_2} = create_page_entry()
-
+    reference = create_table()
+    entry_1 = create_page_entry()
+    entry_2 = create_page_entry()
     Table.put(reference, entry_1)
     Table.put(reference, entry_2)
-    Table.delete_entry(reference, id_1)
+    Table.delete_entry(reference, entry_1.id, entry_1.locale)
 
-    assert Table.get_entry(reference, id_2) == entry_2
-    assert Table.get_entry(reference, id_1) == nil
+    assert [{_, _, ^entry_2}] = :ets.lookup(reference, {:entry, entry_2.id, entry_2.locale})
+    assert [] = :ets.lookup(reference, {:entry, entry_1.id, entry_1.locale})
   end
 
   test "get_entries_for_content_type/2 get filtered entries from table" do
-    name = Faker.Util.format("%A%4a%A%3a") |> String.to_atom()
-    reference = Table.new(name)
+    reference = create_table()
 
-    entry_1 = %{id: id_1} = create_page_entry()
-    entry_2 = %{id: id_2} = create_page_entry()
-    entry_3 = %{id: id_3} = create_simple_page_entry()
+    entries =
+      [entry_1, entry_2, entry_3, entry_4] =
+      [
+        create_page_entry(),
+        create_page_entry(),
+        create_page_entry(locale: :fr),
+        create_simple_page_entry()
+      ]
 
-    Table.put(reference, entry_1)
-    Table.put(reference, entry_2)
-    Table.put(reference, entry_3)
+    Enum.each(entries, &Table.put(reference, &1))
 
-    page_entries = Table.get_entries_for_content_type(reference, :page)
+    page_entries = Table.get_entries_for_content_type(reference, :en, :page)
 
     assert length(page_entries) == 2
-
-    ids = Enum.map(page_entries, & &1.id)
-    assert id_1 in ids
-    assert id_2 in ids
-    assert id_3 not in ids
+    assert entry_1 in page_entries
+    assert entry_2 in page_entries
+    assert entry_3 not in page_entries
+    assert entry_4 not in page_entries
   end
 
   test "get_asset/2 get asset from table" do
-    name = Faker.Util.format("%A%4a%A%3a") |> String.to_atom()
-    reference = Table.new(name)
-
-    asset = %{id: id} = create_asset()
+    reference = create_table()
+    asset = create_asset()
     Table.put(reference, asset)
-
-    assert Table.get_asset(reference, id) == asset
+    assert Table.get_asset(reference, asset.id, asset.locale) == asset
   end
 
   test "delete_asset/2 deletes asset from table" do
-    name = Faker.Util.format("%A%4a%A%3a") |> String.to_atom()
-    reference = Table.new(name)
-
-    asset_1 = %{id: id_1} = create_asset()
-    asset_2 = %{id: id_2} = create_asset()
+    reference = create_table()
+    asset_1 = create_asset()
+    asset_2 = create_asset()
 
     Table.put(reference, asset_1)
     Table.put(reference, asset_2)
-    Table.delete_asset(reference, id_1)
+    Table.delete_asset(reference, asset_1.id, asset_1.locale)
 
-    assert Table.get_asset(reference, id_2) == asset_2
-    assert Table.get_asset(reference, id_1) == nil
+    assert [{_, _, ^asset_2}] = :ets.lookup(reference, {:asset, asset_2.id, asset_2.locale})
+    assert [] = :ets.lookup(reference, {:asset, asset_1.id, asset_1.locale})
   end
 
-  defp create_page_entry() do
+  defp create_table() do
+    Faker.Util.format("%A%4a%A%3a")
+    |> String.to_atom()
+    |> Table.new()
+  end
+
+  defp create_page_entry(opts \\ []) do
+    locale = Keyword.get(opts, :locale, :en)
+
     %Entry{
       store: nil,
       id: Faker.String.base64(10),
       revision: Faker.random_between(1, 1000),
-      space_id: Faker.String.base64(10),
+      space_id: "a_space_id",
       content_type: :page,
-      fields: CFSyncTest.Fields.Page.new({%{}, "en"}),
-      locale: "en"
+      fields: %{},
+      locale: locale
     }
   end
 
@@ -134,10 +124,10 @@ defmodule CFSync.Store.TableTest do
       store: nil,
       id: Faker.String.base64(10),
       revision: Faker.random_between(1, 1000),
-      space_id: Faker.String.base64(10),
+      space_id: "a_space_id",
       content_type: :simple_page,
-      fields: CFSyncTest.Fields.SimplePage.new({%{}, "en"}),
-      locale: "en"
+      fields: %{},
+      locale: :en
     }
   end
 
@@ -145,16 +135,16 @@ defmodule CFSync.Store.TableTest do
     %Asset{
       store: nil,
       id: Faker.String.base64(10),
-      space_id: Faker.String.base64(10),
-      content_type: Faker.String.base64(10),
-      locale: "en",
-      title: Faker.String.base64(10),
-      description: Faker.String.base64(10),
-      file_name: Faker.String.base64(10),
-      url: Faker.Internet.url(),
-      width: 0,
-      height: 0,
-      size: 0
+      space_id: "a_space_id",
+      content_type: "image/jpeg",
+      locale: :en,
+      title: "An image",
+      description: "A cat!!",
+      file_name: "cat.jpg",
+      url: "https://cdn.cats.com/123",
+      width: 10,
+      height: 20,
+      size: 100
     }
   end
 end
