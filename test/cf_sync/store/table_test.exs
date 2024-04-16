8799dd51d3726ee3026b07074bc08bd4fb9218df
Cyprien Poisson <c.poisson@cpi-services.fr>
2024-04-16 21:15:55 +0200

    Table: tests with and without locale handling.

--- test/cf_sync/store/table_test.exs
+++ test/cf_sync/store/table_test.exs
@@ -23,80 +23,177 @@ defmodule CFSync.Store.TableTest do
     assert Keyword.fetch!(info, :write_concurrency) == false
   end
 
-  test "put/2 inserts entries in table" do
-    reference = create_table()
-    entry = create_page_entry()
-    Table.put(reference, entry)
-    assert [{_, _, ^entry}] = :ets.lookup(reference, {:entry, entry.id, entry.locale})
+  describe "With no locale" do
+    test "put/2 inserts entries in table" do
+      reference = create_table()
+      entry = create_page_entry(locale: nil)
+      Table.put(reference, entry)
+      assert [{_, _, ^entry}] = :ets.lookup(reference, {:entry, entry.id, nil})
+    end
+
+    test "put/2 inserts assets in table" do
+      reference = create_table()
+      asset = create_asset(locale: nil)
+      Table.put(reference, asset)
+      assert [{_, _, ^asset}] = :ets.lookup(reference, {:asset, asset.id, nil})
+    end
+
+    test "get_entry/2 get entry from table" do
+      reference = create_table()
+      entry = create_page_entry(locale: nil)
+      Table.put(reference, entry)
+      assert Table.get_entry(reference, entry.id, nil) == entry
+    end
+
+    test "delete_entry/2 deletes entry from table" do
+      reference = create_table()
+      entry_1 = create_page_entry(locale: nil)
+      entry_2 = create_page_entry(locale: nil)
+      Table.put(reference, entry_1)
+      Table.put(reference, entry_2)
+      Table.delete_entry(reference, entry_1.id, nil)
+
+      assert [{_, _, ^entry_2}] = :ets.lookup(reference, {:entry, entry_2.id, nil})
+      assert [] = :ets.lookup(reference, {:entry, entry_1.id, nil})
+    end
+
+    test "get_entries_for_content_type/2 get filtered entries from table" do
+      reference = create_table()
+
+      entries =
+        [entry_1, entry_2, entry_3] =
+        [
+          create_page_entry(locale: nil),
+          create_page_entry(locale: nil),
+          create_simple_page_entry(locale: nil)
+        ]
+
+      Enum.each(entries, &Table.put(reference, &1))
+
+      page_entries = Table.get_entries_for_content_type(reference, nil, :page)
+
+      assert length(page_entries) == 2
+      assert entry_1 in page_entries
+      assert entry_2 in page_entries
+      assert entry_3 not in page_entries
+    end
+
+    test "get_asset/2 get asset from table" do
+      reference = create_table()
+      asset = create_asset(locale: nil)
+      Table.put(reference, asset)
+      assert Table.get_asset(reference, asset.id, nil) == asset
+    end
+
+    test "delete_asset/2 deletes asset from table" do
+      reference = create_table()
+      asset_1 = create_asset(locale: nil)
+      asset_2 = create_asset(locale: nil)
+
+      Table.put(reference, asset_1)
+      Table.put(reference, asset_2)
+      Table.delete_asset(reference, asset_1.id, nil)
+
+      assert [{_, _, ^asset_2}] = :ets.lookup(reference, {:asset, asset_2.id, nil})
+      assert [] = :ets.lookup(reference, {:asset, asset_1.id, nil})
+    end
   end
 
-  test "put/2 inserts assets in table" do
-    reference = create_table()
-    asset = create_asset()
-    Table.put(reference, asset)
-    assert [{_, _, ^asset}] = :ets.lookup(reference, {:asset, asset.id, asset.locale})
-  end
+  describe "With locales" do
+    test "put/2 inserts entries in table" do
+      reference = create_table()
+      entry_fr = create_page_entry(locale: :fr)
+      entry_en = create_page_entry(locale: :en)
+      Table.put(reference, entry_fr)
+      Table.put(reference, entry_en)
+      assert [{_, _, ^entry_fr}] = :ets.lookup(reference, {:entry, entry_fr.id, :fr})
+      assert [{_, _, ^entry_en}] = :ets.lookup(reference, {:entry, entry_en.id, :en})
+    end
 
-  test "get_entry/2 get entry from table" do
-    reference = create_table()
-    entry = create_page_entry()
-    Table.put(reference, entry)
-    assert Table.get_entry(reference, entry.id, entry.locale) == entry
-  end
+    test "put/2 inserts assets in table" do
+      reference = create_table()
+      asset_de = create_asset(locale: :de)
+      asset_it = create_asset(locale: :it)
+      Table.put(reference, asset_de)
+      Table.put(reference, asset_it)
+      assert [{_, _, ^asset_de}] = :ets.lookup(reference, {:asset, asset_de.id, :de})
+      assert [{_, _, ^asset_it}] = :ets.lookup(reference, {:asset, asset_it.id, :it})
+    end
 
-  test "delete_entry/2 deletes entry from table" do
-    reference = create_table()
-    entry_1 = create_page_entry()
-    entry_2 = create_page_entry()
-    Table.put(reference, entry_1)
-    Table.put(reference, entry_2)
-    Table.delete_entry(reference, entry_1.id, entry_1.locale)
+    test "get_entry/2 get entry from table" do
+      reference = create_table()
+      entry_pl = create_page_entry(locale: :pl)
+      entry_pt = create_page_entry(locale: :pt)
+      Table.put(reference, entry_pl)
+      Table.put(reference, entry_pt)
+      assert Table.get_entry(reference, entry_pl.id, :pl) == entry_pl
+      assert Table.get_entry(reference, entry_pt.id, :pt) == entry_pt
+    end
 
-    assert [{_, _, ^entry_2}] = :ets.lookup(reference, {:entry, entry_2.id, entry_2.locale})
-    assert [] = :ets.lookup(reference, {:entry, entry_1.id, entry_1.locale})
-  end
+    test "delete_entry/2 deletes entry from table" do
+      reference = create_table()
+      entry_en1 = create_page_entry(locale: :en)
+      entry_en2 = create_page_entry(locale: :en)
+      entry_es1 = create_page_entry(locale: :es)
+      Table.put(reference, entry_en1)
+      Table.put(reference, entry_en2)
+      Table.put(reference, entry_es1)
+      Table.delete_entry(reference, entry_en1.id, :en)
 
-  test "get_entries_for_content_type/2 get filtered entries from table" do
-    reference = create_table()
+      assert [{_, _, ^entry_en2}] = :ets.lookup(reference, {:entry, entry_en2.id, :en})
+      assert [{_, _, ^entry_es1}] = :ets.lookup(reference, {:entry, entry_es1.id, :es})
+      assert [] = :ets.lookup(reference, {:entry, entry_en1.id, :en})
+    end
 
-    entries =
-      [entry_1, entry_2, entry_3, entry_4] =
-      [
-        create_page_entry(),
-        create_page_entry(),
-        create_page_entry(locale: :fr),
-        create_simple_page_entry()
-      ]
+    test "get_entries_for_content_type/2 get filtered entries from table" do
+      reference = create_table()
 
-    Enum.each(entries, &Table.put(reference, &1))
+      entries =
+        [entry_fr1 | [entry_fr2 | _]] =
+        [
+          create_page_entry(locale: :fr),
+          create_page_entry(locale: :fr),
+          create_simple_page_entry(locale: :fr),
+          create_page_entry(locale: :it),
+          create_page_entry(locale: :it),
+          create_simple_page_entry(locale: :it)
+        ]
 
-    page_entries = Table.get_entries_for_content_type(reference, :en, :page)
+      Enum.each(entries, &Table.put(reference, &1))
 
-    assert length(page_entries) == 2
-    assert entry_1 in page_entries
-    assert entry_2 in page_entries
-    assert entry_3 not in page_entries
-    assert entry_4 not in page_entries
-  end
+      fr_page_entries = Table.get_entries_for_content_type(reference, :fr, :page)
 
-  test "get_asset/2 get asset from table" do
-    reference = create_table()
-    asset = create_asset()
-    Table.put(reference, asset)
-    assert Table.get_asset(reference, asset.id, asset.locale) == asset
-  end
+      assert length(fr_page_entries) == 2
+      assert entry_fr1 in fr_page_entries
+      assert entry_fr2 in fr_page_entries
+    end
 
-  test "delete_asset/2 deletes asset from table" do
-    reference = create_table()
-    asset_1 = create_asset()
-    asset_2 = create_asset()
+    test "get_asset/2 get asset from table" do
+      reference = create_table()
+      asset_de = create_asset(locale: :de)
+      asset_nl = create_asset(locale: :nl)
+      Table.put(reference, asset_de)
+      Table.put(reference, asset_nl)
+      assert Table.get_asset(reference, asset_de.id, :de) == asset_de
+      assert Table.get_asset(reference, asset_nl.id, :nl) == asset_nl
+    end
 
-    Table.put(reference, asset_1)
-    Table.put(reference, asset_2)
-    Table.delete_asset(reference, asset_1.id, asset_1.locale)
+    test "delete_asset/2 deletes asset from table" do
+      reference = create_table()
+      asset_fr1 = create_asset(locale: :fr)
+      asset_fr2 = create_asset(locale: :fr)
+      asset_pl1 = create_asset(locale: :pl)
 
-    assert [{_, _, ^asset_2}] = :ets.lookup(reference, {:asset, asset_2.id, asset_2.locale})
-    assert [] = :ets.lookup(reference, {:asset, asset_1.id, asset_1.locale})
+      Table.put(reference, asset_fr1)
+      Table.put(reference, asset_fr2)
+      Table.put(reference, asset_pl1)
+
+      Table.delete_asset(reference, asset_fr1.id, :fr)
+
+      assert [{_, _, ^asset_fr2}] = :ets.lookup(reference, {:asset, asset_fr2.id, :fr})
+      assert [{_, _, ^asset_pl1}] = :ets.lookup(reference, {:asset, asset_pl1.id, :pl})
+      assert [] = :ets.lookup(reference, {:asset, asset_fr1.id, :fr})
+    end
   end
 
   defp create_table() do
@@ -105,8 +202,8 @@ defmodule CFSync.Store.TableTest do
     |> Table.new()
   end
 
-  defp create_page_entry(opts \\ []) do
-    locale = Keyword.get(opts, :locale, :en)
+  defp create_page_entry(opts) do
+    locale = Keyword.fetch!(opts, :locale)
 
     %Entry{
       store: nil,
@@ -119,7 +216,9 @@ defmodule CFSync.Store.TableTest do
     }
   end
 
-  defp create_simple_page_entry() do
+  defp create_simple_page_entry(opts) do
+    locale = Keyword.fetch!(opts, :locale)
+
     %Entry{
       store: nil,
       id: Faker.String.base64(10),
@@ -127,17 +226,19 @@ defmodule CFSync.Store.TableTest do
       space_id: "a_space_id",
       content_type: :simple_page,
       fields: %{},
-      locale: :en
+      locale: locale
     }
   end
 
-  defp create_asset() do
+  defp create_asset(opts) do
+    locale = Keyword.fetch!(opts, :locale)
+
     %Asset{
       store: nil,
       id: Faker.String.base64(10),
       space_id: "a_space_id",
       content_type: "image/jpeg",
-      locale: :en,
+      locale: locale,
       title: "An image",
       description: "A cat!!",
       file_name: "cat.jpg",
