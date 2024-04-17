6f53a8a4ab0e37a90910e794551c6857ec0bb52f
Cyprien Poisson <c.poisson@cpi-services.fr>
2024-04-17 15:52:29 +0200

    Typo.

--- lib/cf_sync/store/state.ex
+++ lib/cf_sync/store/state.ex
@@ -2,7 +2,6 @@ defmodule CFSync.Store.State do
   @moduledoc false
 
   @default_root_url "https://cdn.contentful.com/"
-  @default_locale nil
   @default_locales %{nil => "en-US"}
   @default_initial_sync_interval 30
   @default_delta_sync_interval 5000
